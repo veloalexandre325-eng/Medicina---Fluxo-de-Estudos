a483b9af6fff1a0bcdb2172a45468789a32e20ed
veloalexandre325-eng <veloalexandre325@uni9.edu.br>
2026-09-18 14:31:42 -0300

    11ª versão

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="medicina-v35-mahjong-visible-bank";
+const CACHE="medicina-v36-mahjong-powers-fluid";
 const CORE=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
